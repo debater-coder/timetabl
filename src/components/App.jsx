492eaf0daebe79fe5b3b9ff8b5d6cf95e878fafc
debater-coder <hamzah@syedahmed.net>
2021-07-22 16:09:47 +1000

    Add console log

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,15 @@ import User from '../User';
 let App = () => {
   let [loggedIn, setLoggedIn] = useState(false)
   let [user] = useState(new User(setLoggedIn));
+  let participation = user.apiRequest("details/participation")
+  console.log(participation)
   return (
     <div className='App'>
       {
         loggedIn ? <button onClick={user.logout}>Log out</button> : <button onClick={user.login}>Log in</button>
       }
       <p>Hello, {loggedIn ? user.apiRequest("details/userinfo")["givenName"] : "Anonymous"}</p>
-      {loggedIn ? <p>You have {user.apiRequest("details/participation")[2]["points"]} award scheme points</p> : <p>You are not logged in</p>}
+      {loggedIn ? <p>You have {participation.pop()["points"]} award scheme points</p> : <p>You are not logged in</p>}
     </div>
   );
 };
