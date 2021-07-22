0d543fa6faeea1ac1d32aeb615411d43d7c45600
debater-coder <hamzah@syedahmed.net>
2021-07-22 20:39:06 +1000

    Add index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,8 +36,7 @@ let App = () => {
     if (loggedIn) {
       apiRequest('details/participation')
         .then(data => {
-          console.log(data.splice(-1))
-          setPoints(data.splice(-1).points)
+          setPoints(data.splice(-1)[0].points)
         })
     }
   }, [loggedIn])
