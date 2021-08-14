63728b1cc1024ad0c00a7bd979d2af0646fa23ab
debater-coder <52619668+debater-coder@users.noreply.github.com>
2021-08-14 21:35:58 +1000

    Update Proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,9 +1,61 @@
 const fetch = require('node-fetch');
+const cookie = require('cookie')
 
-exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
-  return {
-    statusCode: 200,
-    body: `Hello ${subject}!`,
+const config = {
+  client_id: 'timetabl',
+  redirect_uri: 'https://sbhs-timetabl.netlify.app/',
+  authorization_endpoint: 'https://student.sbhs.net.au/api/authorize',
+  token_endpoint: 'https://student.sbhs.net.au/api/token',
+  api_endpoint: 'https://student.sbhs.net.au/api',
+  scopes: 'all-ro',
+};
+
+exports.handler = async (event) => {
+  try {
+    let res = await fetch(config.token_endpoint, {
+      method: "POST",
+      body: new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: JSON.parse(event.body)["code"],
+        client_id: config.client_id,
+        redirect_uri: config.redirect_uri,
+        code_verifier: JSON.parse(event.body)["verifier"],
+      }),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+      }
+    })
+
+    if (!res.ok) {
+      return { statusCode: res.status, body: res.statusText }
+    }
+
+    let json = res.json()
+
+    return {
+      statusCode: 200,
+      multiValueHeaders: {
+        "Set-Cookie": [
+          cookie.serialize("access_token", json["access_token"], {
+            httpOnly: true,
+            path: "/",
+            sameSite: 'lax',
+            secure: true
+          }),
+          cookie.serialize("refresh_token", json["refresh_token"], {
+            httpOnly: true,
+            path: "/",
+            sameSite: 'lax',
+            secure: true
+          })
+        ]
+      }
+    }
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: error.message })
+    }
   }
 }
