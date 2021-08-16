463ade9a1df9dc3d144aee2338d13665e2eff0bd
debater-coder <52619668+debater-coder@users.noreply.github.com>
2021-08-16 21:21:59 +1000

    Change function name

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
   client_id: 'timetabldev',
-  redirect_uri: 'http://localhost:8080/',
+  redirect_uri: 'http://localhost:8888/',
   authorization_endpoint: 'https://student.sbhs.net.au/api/authorize',
-  proxy_endpoint: "http://localhost:8080/proxy",
+  auth_endpoint: 'http://localhost:8888/.netlify/functions/auth',
   scopes: 'all-ro',
 };
