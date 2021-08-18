68bb6a40fcbad79314bac510aea7e8cf6f0e1d14
debater-coder <52619668+debater-coder@users.noreply.github.com>
2021-08-18 18:34:04 +1000

    Add schema and resolvers

--- api/api/api.js
+++ api/api/api.js
@@ -1,42 +1,60 @@
-const { ApolloServer, gql } = require('apollo-server-lambda')
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-    allAuthors: [Author!]
-    author(id: Int!): Author
-    authorByName(name: String!): Author
-  }
-  type Author {
-    id: ID!
-    name: String!
-    married: Boolean!
-  }
-`
-
-const authors = [
-  { id: 1, name: 'Terry Pratchett', married: false },
-  { id: 2, name: 'Stephen King', married: true },
-  { id: 3, name: 'JK Rowling', married: false },
-]
+const { ApolloServer } = require('apollo-server-lambda')
+const path = require('path');
+const fs = require('fs');
+const fetch = require('node-fetch');
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello, world!',
-    allAuthors: () => authors,
-    author: () => {},
-    authorByName: (root, args) => {
-      console.log('hihhihi', args.name)
-      return authors.find((author) => author.name === args.name) || 'NOTFOUND'
-    },
+    info: () => "This is a GraphQL wrapper for the student portal API",
+
+    today: () => fetch("https://student.sbhs.net.au/api/calendar/days.json")
+      .then(res => res.json())
+      .then(data => data[Object.keys(data)[0]]),
+
+    day: (parent, args) => fetch("https://student.sbhs.net.au/api/calendar/days.json?from=" + args.date)
+      .then(res => res.json())
+      .then(data => data[Object.keys(data)[0]]),
+
+    terms: () => fetch("https://student.sbhs.net.au/api/calendar/terms.json")
+      .then(res => res.json())
+      .then(data => Object.values(data["terms"])),
+
+    publicHolidays: () => fetch("https://student.sbhs.net.au/api/calendar/terms.json")
+      .then(res => res.json())
+      .then(data => Object.entries(data['publicHolidays'])),
+
+    developmentDays: () => fetch("https://student.sbhs.net.au/api/calendar/terms.json")
+      .then(res => res.json())
+      .then(data => Object.entries(data["developmentDays"])),
+
+    bellsToday: () => fetch("https://student.sbhs.net.au/api/timetable/bells.json")
+      .then(res => res.json()),
+
+    bells: (parent, args) => fetch("https://student.sbhs.net.au/api/timetable/bells.json?date=" + args.date)
+      .then(res => res.json()),
   },
+  DayOff: {
+    date: parent => parent[0],
+    name: parent => parent[1]
+  }
 }
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
   resolvers,
 })
 
+const runHandler = (event, context, handler) =>
+  new Promise((resolve, reject) => {
+    const callback = (error, body) => (error ? reject(error) : resolve(body));
+    handler(event, context, callback);
+});
+
 const handler = server.createHandler()
 
-module.exports = { handler }
+module.exports.handler = async (event, context) => {
+  return await runHandler(event, context, handler)
+}
