e372c311311f4de3f1ff7b8cca4fb2e0629d8b8b
debater-coder <52619668+debater-coder@users.noreply.github.com>
2021-08-22 17:52:27 +1000

    Switch to prod environment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
-  client_id: 'timetabldev',
-  redirect_uri: 'http://localhost:8888/',
+  client_id: 'timetabl',
+  redirect_uri: 'https://sbhs-timetabl.netlify.app/',
   authorization_endpoint: 'https://student.sbhs.net.au/api/authorize',
-  auth_endpoint: 'http://localhost:8888/.netlify/functions/auth',
+  auth_endpoint: 'https://sbhs-timetabl.netlify.app/.netlify/functions/auth',
   scopes: 'all-ro',
 };
