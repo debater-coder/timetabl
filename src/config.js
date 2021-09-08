d7b4013efb8f18ab5dca758fa4cdd3a55254f900
debater-coder <52619668+debater-coder@users.noreply.github.com>
2021-09-08 17:28:25 +1000

    Switch to dev environment

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
-  client_id: 'timetabl',
-  redirect_uri: 'https://sbhs-timetabl.netlify.app/',
-  authorization_endpoint: 'https://student.sbhs.net.au/api/authorize',
-  auth_endpoint: 'https://sbhs-timetabl.netlify.app/.netlify/functions/auth',
-  api_endpoint: 'https://sbhs-timetabl.netlify.app/.netlify/functions/api',
+  client_id: 'timetabldev',
+  redirect_uri: 'http://localhost:8888/',
+  authorization_endpoint: 'http://localhost:8888/api/authorize',
+  auth_endpoint: 'http://localhost:8888/.netlify/functions/auth',
+  api_endpoint: 'http://localhost:8888/.netlify/functions/api',
   scopes: 'all-ro',
 };
