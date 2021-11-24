391cdf9dda45d31d0aa0dabe4f8d718b564da9f9
debater-coder <52619668+debater-coder@users.noreply.github.com>
2021-11-25 09:40:18 +1100

    TIM-12 Build App Shell

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -9,8 +9,7 @@ export default {
       '@snowpack/plugin-webpack',
     ],
   ],
-  routes: [
-  ],
+  routes: [],
   optimize: {
     /* Example: Bundle your final build: */
     // "bundle": true,
