ebc401714e1f6b183f397715771093b5cea85c79
debater-coder <52619668+debater-coder@users.noreply.github.com>
2021-12-27 16:48:06 +1100

    Create helper library

---
+++ src/contextualise/src/contextualise.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useContext } from 'react';
+import theme from '../theme';
+
+export default (hook, config, defaultValue) => {
+  const Context = createContext(defaultValue)
+
+  const Provider = ({children}) =>
+    <Context.Provider value={hook(...config)}>
+      {children}
+    </Context.Provider>
+
+  return [() => useContext(Context), Provider]
+}
+
+// Credit to: https://stackoverflow.com/questions/51504506/too-many-react-context-providers
+export const Compose = ({components= [], children}) =>  <>
+  {components.reduceRight((acc, Comp) => {
+    return <Comp>{acc}</Comp>
+  }, children)}
+</>
+
+export const withProps = (Component, addedProps) => {
+  return props => <Component {...props} {...addedProps} />
+}
