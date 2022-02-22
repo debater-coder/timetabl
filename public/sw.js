58c7ed58a67f99e437080b6adf96c4373d92882f
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-02-22 17:27:47 +1100

    PWA again

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+// This code executes in its own worker or thread
+self.addEventListener("install", event => {
+  console.log("Service worker installed");
+});
+self.addEventListener("activate", event => {
+  console.log("Service worker activated");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => new Response("U R OFFLINE")))
+})
--- src/index.jsx
+++ src/index.jsx
@@ -30,3 +30,14 @@ ReactDOM.render(
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register("/sw.js", {scope: '/'})
+    .then((reg) => {
+      // registration worked
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    }).catch((error) => {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
