986c5de81a69cdf6e76b2534a73b3203f138d4e1
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-03-05 23:05:08 +1100

    More Padding in log

--- src/index.jsx
+++ src/index.jsx
@@ -36,7 +36,7 @@ console.log(
   "background-color: #0093E9;" +
   "background-image: linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);" +
   "border-radius: 32px;" +
-  "padding: 4px;"
+  "padding: 8px;"
 )
 
 if ('serviceWorker' in navigator) {
