0b96128595ba226f1be94ae7d928ce913c1b4ad8
debater-coder <52619668+debater-coder@users.noreply.github.com>
2022-03-24 16:03:59 +1100

    Login to school website

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
   client_id: import.meta.env.VITE_CLIENT_ID,
-  redirect_uri: import.meta.url,
+  redirect_uri: window.location.href,
   authorization_endpoint: "https://student.sbhs.net.au/api/authorize",
   auth_endpoint: import.meta.env.VITE_AUTH_ENDPOINT,
   api_endpoint: import.meta.env.API_ENDPOINT,
-  scopes: import.meta.env.VITE_SCOPES,
+  scopes: "all-ro",
 };
